642cba535ffb39d1ad5f0785ea78c20d88e591ee
jsoucy <jsoucy@debian-BULLSEYE-live-builder-AMD64>
2022-07-14 13:40:57 -0400

    Première modification

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,6 @@ release = 'Université Laval'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
 autosectionlabel_prefix_document = True
 autosectionlabel_maxdepth = 4
 # Add any paths that contain templates here, relative to this directory.
